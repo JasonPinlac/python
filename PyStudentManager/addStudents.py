b78fb6fe313afc45c910e6b413f30f42ab19ae36
Jason Pinlac <jasonpinlac@gmail.com>
2018-08-13 21:32:22 -0400

    read and save persistence

--- PyStudentManager/addStudents.py
+++ PyStudentManager/addStudents.py
@@ -7,7 +7,7 @@ def add_student(name, student_id):
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase.append(student["nameee"].title())
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -23,8 +23,25 @@ def prompt_for_students():
         if(response == "n"):
             break
 
-try:    
-    prompt_for_students()
-    print_students_titlecase()  
-except Exception:
-    print("unknown, general error")
+def read_file():
+    f = open("students.txt", "r")
+    for student in f.readlines():
+        student_details = student.split(",")
+        print(student_details)
+        if len(student_details) > 1:
+            add_student(student_details[0], student_details[1].strip('\n'))
+    f.close()
+
+def save_file():
+    f = open("students.txt", "w") # a is an access mode. There is r, a, w and more
+    for student in students:
+        f.write("{0},{1}\n".format(student["name"], student["student_id"]))
+    f.close()
+
+# try:
+read_file()
+print(students)
+prompt_for_students()
+save_file()
+# except Exception as error:
+#     print("Error: {0}".format(error))
