669e368698fcec73f70829625e822af436c11705
Jason Pinlac <jason.pinlac@dycominc.com>
2018-08-20 17:38:51 -0400

    classes, inheritance and polymorphism

--- gettingStarted/classes.py
+++ gettingStarted/classes.py
@@ -1,40 +1,39 @@
 
 class Student:
+    # static attribute
+    school_name = "Jupiter Elementary"
 
-    # static variables
-    school_name = "Jupiter High School"
-
-    # static methods
-    def get_school():
-        return "Jupiter High School"
-
-    # consctuctor
+    # constructor
     def __init__(self, name, student_id="0"):
         # instance attributes
         self.name = name
         self.student_id = student_id
-    
+    # overridden __str__ method that is output on print()
     def __str__(self):
-        return self.name + " " + self.student_id
+        return f"Student object:\n\tname: {self.name}\n\tstudent_id: {self.student_id}\n\t{self.school_name}"
 
     # instance methods
     def get_name_capitalized(self):
         return self.name.capitalize()
-
-  
-
-def add_student(student):
-    students.append(student)
-
-students = []
-
-student1 = Student("jason", "7")
+    
+    def get_school_name(self):
+        return self.school_name
 
 
-print(student1.name + student1.student_id)
+class HighSchoolStudent(Student):
 
-print(student1.get_name_capitalized())
+    def __str__(self):
+        return f"HighSchoolStudent object:\n\tname: {self.name}\n\tstudent_id: {self.student_id}\n\t{self.school_name}"
 
+    def get_school_name(self):
+        org_val = super().get_school_name()
+        return org_val + "-HS"
+        
+student1 = Student("Jason", 1)
+student2 = HighSchoolStudent("Mike", 2)
 
-print(Student.get_school())
-print(Student.school_name)
+# print(student1)
+# print(student2)
+
+print(student1.get_school_name())
+print(student2.get_school_name())
